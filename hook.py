1f2b66b14adbb955314c3285605633435a82e217
Aledangelo <alessandro.dangelo13.99@gmail.com>
2022-11-04 19:57:51 +0100

    problem with GUI

--- hook.py
+++ hook.py
@@ -1,27 +1,28 @@
-from aiohttp_jinja2 import template, web
+from aiohttp_jinja2 import template
 from app.service.auth_svc import check_authorization
 
 name = "misp"
 description = "Plugin to load event from MISP to Caldera"
-address = '/plugin/misp/gui'
+address = "/plugin/misp/gui"
 
-async def enable(services):
+def enable(services):
     app = services.get('app_svc').application
-    fetcher = AbilityFetcher(services)
-    app.router.add_route('*', '/get/abilities', fetcher.get_abilities)
-    app.router.add_route('*', '/plugin/misp/gui', fetcher.splash)
+    misp = MispLoader(services)
+    app.router.add_route('*', '/plugin/misp/gui', misp.splash)
+    app.router.add_route('POST', '/plugin/misp/start', misp.start)
 
-class AbilityFetcher:
+
+class MispLoader:
     def __init__(self, services):
         self.services = services
         self.auth_svc = services.get('auth_svc')
 
-    async def get_abilities(self, request):
-        abilities = await self.services.get('data_svc').locate('abilities')
-        return web.json_response(dict(abilities=[a.display for a in abilities]))
-
     @check_authorization
     @template('misp.html')
     async def splash(self, request):
-        abilities = await self.services.get('data_svc').locate('abilities')
-        return(dict(abilities=[a.display for a in abilities]))
+        return(dict())
+
+    @check_authorization
+    async def start(self, request):
+        data = await request.json()
+        
